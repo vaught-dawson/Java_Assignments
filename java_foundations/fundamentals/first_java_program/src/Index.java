e24d27966d69b99949ee6bcafcd32006d018d282
Dawson Vaught <vaught.dawson@gmail.com>
2022-06-06 11:33:03 -0700

    first commit

---
+++ java_foundations/fundamentals/first_java_program/src/Index.java
@@ -0,0 +1,7 @@
+class Index {
+	public static void main(String[] args) {
+		System.out.println("My name is Dawson Vaught");
+		System.out.println("I am 20 years old");
+		System.out.println("My hometown is Phoenix, Arizona");
+	}
+}

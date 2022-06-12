a5f67cabb9e1f72ca8426f944361a6dd11fd9a19
Dawson Vaught <vaught.dawson@gmail.com>
2022-06-12 09:58:02 -0700

    add a new assignment CareSoft Interfaces

---
+++ java_oop/java_oop_advanced/caresoft_interfaces/src/com/caresoft/clinicapp/Physician.java
@@ -0,0 +1,48 @@
+package com.caresoft.clinicapp;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Physician extends User implements HIPAACompliantUser {
+	private ArrayList<String> patientNotes;
+
+	public Physician(int id) {
+		super(id);
+	}
+
+	public ArrayList<String> getPatientNotes() {
+		return patientNotes;
+	}
+
+	public void setPatientNotes(ArrayList<String> patientNotes) {
+		this.patientNotes = patientNotes;
+	}
+
+	public void newPatientNotes(String notes, String patientName, Date date) {
+		String report = String.format("Datetime Submitted: %s \n", date);
+		report += String.format("Reported By ID: %s\n", this.id);
+		report += String.format("Patient Name: %s\n", patientName);
+		report += String.format("Notes: %s \n", notes);
+		this.patientNotes.add(report);
+	}
+
+	@Override
+	public boolean assignPin(int pin) {
+		if (String.valueOf(pin).length() != 4) {			
+			return false;
+		}
+		
+		this.pin = pin;
+		return true;
+	}
+
+	@Override
+	public boolean accessAuthorized(Integer confirmedAuthID) {
+		if (confirmedAuthID == this.id) {
+			return true;
+		}
+		
+		return false;
+	}
+
+}

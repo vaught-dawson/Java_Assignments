044bc76d90eb46fb28c86fdf938fa8e0aac99910
Dawson Vaught <vaught.dawson@gmail.com>
2022-06-12 16:54:00 -0700

    add a new assignment Daikichi Routes

---
+++ java_spring/spring_intro/daikichi_routes/src/main/java/dev/dawsonvaught/daikichiroutes/DaikichiController.java
@@ -0,0 +1,24 @@
+package dev.dawsonvaught.daikichiroutes;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("daikichi")
+public class DaikichiController {
+	
+	@RequestMapping("")
+	public String index() {
+		return "Welcome!";
+	}
+	
+	@RequestMapping("/today")
+	public String today() {
+		return "Today you will find luck in all your endeavors!";
+	}
+	
+	@RequestMapping("tomorrow")
+	public String tomorrow() {
+		return "Tomorrow, an opportinity will arise, so be sure to be open to new ideas!";
+	}
+}

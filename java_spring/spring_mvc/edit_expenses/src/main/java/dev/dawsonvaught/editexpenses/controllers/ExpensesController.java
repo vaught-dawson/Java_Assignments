2e9c9222e4792b5ec2dfbd7ff73516f5424f30d3
Dawson Vaught <vaught.dawson@gmail.com>
2022-06-21 12:29:05 -0700

    Minor Changes

--- java_spring/spring_mvc/edit_expenses/src/main/java/dev/dawsonvaught/editexpenses/controllers/ExpensesController.java
+++ java_spring/spring_mvc/edit_expenses/src/main/java/dev/dawsonvaught/editexpenses/controllers/ExpensesController.java
@@ -39,7 +39,7 @@ public class ExpensesController {
 
 	@PostMapping("/expenses")
 	public String create(@Valid @ModelAttribute("expense") Expense expense, BindingResult result) {
-		
+
 		if (!result.hasErrors()) {
 			expenseService.create(expense);
 			return "redirect:/expenses";
@@ -47,15 +47,16 @@ public class ExpensesController {
 
 		return "/expenses/index.jsp";
 	}
-	
+
 	@GetMapping("/expenses/edit/{id}")
 	public String editDisplay(Model model, @PathVariable("id") long id) {
 		model.addAttribute("expense", expenseService.find(id));
 		return "/expenses/edit.jsp";
 	}
-	
+
 	@PutMapping("/expenses/{id}/edit")
-	public String editPut(@Valid @ModelAttribute("expense") Expense expense, BindingResult result, @PathVariable("id") long id) {
+	public String editPut(@Valid @ModelAttribute("expense") Expense expense, BindingResult result,
+			@PathVariable("id") long id) {
 		if (!result.hasErrors()) {
 			expense.setId(id);
 			expenseService.update(expense);
